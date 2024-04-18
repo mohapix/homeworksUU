898f060907125324931608bb107fd760456207a5
mohapix <166431661+mohapix@users.noreply.github.com>
2024-04-18 10:27:12 +0300

    Add files via upload

---
+++ homework0.py
@@ -0,0 +1,7 @@
+string1 = "это моя строка"
+print(string1[0])
+print(string1[-1])
+print(string1[2:5])
+print(string1[::-1])
+print(len(string1))
+print("это новая строка " + string1)
---
+++ homework1.py
@@ -0,0 +1,7 @@
+string = "это моя строка"
+print(string[0])
+print(string[-1])
+print(string[2:5])
+print(string[::-1])
+print(len(string))
+print("это новая строка " + string)
