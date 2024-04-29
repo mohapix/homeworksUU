f9c167ff5fc15928e2e86187c19125db63f8baea
mohapix <166431661+mohapix@users.noreply.github.com>
2024-04-29 14:51:36 +0300

    Add files via upload

--- homework3.py
+++ homework3.py
@@ -1,7 +1,5 @@
-my_string = input("Введите данные: ")
-print(len(my_string))
-print(my_string.upper())
-print(my_string.lower())
-my_string = my_string.replace(" ", "")
-print(my_string[0])
-print(my_string[-1])
+list_ = ["BMW", "MB", "LADA", "KIA", "HONDA"]
+cars_count = 0
+for i in range(len(list_)):
+    print("Я езжу на автомобиле марки", list_[i])
+    cars_count += 10
