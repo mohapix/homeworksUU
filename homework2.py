2b91f9b758b1e964bacab27caf2d0aa959fd63fd
mohapix <166431661+mohapix@users.noreply.github.com>
2024-04-29 12:24:56 +0300

    Add files via upload

--- homework2.py
+++ homework2.py
@@ -1,8 +1,24 @@
-name = "Pavel"
-print("Name:", name)
-age = 40
-print("Age:", age)
-age = age + 1
-print("New age:", age)
-is_student = True
-print("Is Student:", is_student)
+num1 = int(input("Введите целое число: "))
+print("Время суток?", end=' ')
+if 5 <= num1 <= 11:
+    print("Утро", end='\n')
+elif 12 <= num1 <= 17:
+    print("День", end='\n')
+elif 18 <= num1 <= 22:
+    print("Вечер", end='\n')
+elif (0 <= num1 <= 4) or (23 <= num1 <= 24):
+    print("Ночь", end='\n')
+else:
+    print("Ошибка", end='\n')
+
+if '34' > '123':
+    print("успех")
+
+if '123' > '12':
+    print("успех")
+
+if [1, 2] > [1, 1]:
+    print("успех")
+
+if '6' != 5:
+    print("успех")
