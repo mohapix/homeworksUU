2c0162dcfe5d9a9f39bddc0903f6d94f45c0bbec
mohapix <166431661+mohapix@users.noreply.github.com>
2024-05-02 09:42:30 +0300

    Add files via upload

--- homework2.py
+++ homework2.py
@@ -1,8 +1,15 @@
-name = "Pavel"
-print("Name:", name)
-age = 40
-print("Age:", age)
-age = age + 1
-print("New age:", age)
-is_student = True
-print("Is Student:", is_student)
+def print_params(a=1, b='default string', c=True):
+    print(f"Параметр 'a': {a}, параметр 'b': {b}, параметр 'c': {c}")
+
+
+print_params()
+print_params(b=25)
+print_params(c=[1,2,3])
+
+values_list = ['This is values_list', 50, [10, 20]]
+values_dict = {'a': False, 'b': 100, 'c': 'This is values_dict'}
+print_params(*values_list)
+print_params(**values_dict)
+
+values_list_2 = [[0, True], 'This is values_list_2']
+print_params(*values_list_2, 42)
