7e4a654b0ab24620a58b00044ec8f0693e4b6c6f
mohapix <166431661+mohapix@users.noreply.github.com>
2024-05-21 00:41:23 +0300

    Add files via upload

--- homework2.py
+++ homework2.py
@@ -1,15 +1,11 @@
-def print_params(a=1, b='default string', c=True):
-    print(f"Параметр 'a': {a}, параметр 'b': {b}, параметр 'c': {c}")
+def test_function():
+    def inner_function():
+        print("Я в области видимости функции test_function")
+
+    inner_function()
 
 
-print_params()
-print_params(b=25)
-print_params(c=[1,2,3])
-
-values_list = ['This is values_list', 50, [10, 20]]
-values_dict = {'a': False, 'b': 100, 'c': 'This is values_dict'}
-print_params(*values_list)
-print_params(**values_dict)
-
-values_list_2 = [[0, True], 'This is values_list_2']
-print_params(*values_list_2, 42)
+try:
+    inner_function()
+except NameError:
+    print("Функция не объявлена")
