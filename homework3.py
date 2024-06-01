99f0e0b75123f1f12fa66a118375b86b805ea70e
mohapix <166431661+mohapix@users.noreply.github.com>
2024-06-01 15:16:29 +0300

    Add files via upload

--- homework3.py
+++ homework3.py
@@ -1,17 +1,17 @@
-def test(a=10, b='String', c=True):
-    print(a, b, c)
-    return
+class Building:
+
+    def __init__(self, floors, type):
+        self.numberOfFloors = floors
+        self.buildingType = type
+
+    def __eq__(self, other):
+        return self.numberOfFloors == other.numberOfFloors and self.buildingType == other.buildingType
 
 
-test()
-print()
-
-
-def faktorial(n):
-    if n == 1:
-        return 1
-    return n * faktorial(n - 1)
-
-
-num = int(input("Задайте число: "))
-print("Факториал заданного числа:", faktorial(num))
+h1 = Building(10, 'Многоэтажный дом')
+h2 = Building(10, 'Многоэтажный дом')
+h3 = Building(9, 'Многоэтажный дом')
+h4 = Building(10, 'Многоэтажное строение')
+print(h1 == h2)
+print(h2 == h3)
+print(h1 == h4)
