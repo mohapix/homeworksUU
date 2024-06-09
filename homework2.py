39da798cfde46947ceda95b4705f656b30b0a763
mohapix <166431661+mohapix@users.noreply.github.com>
2024-06-09 11:52:37 +0300

    Add files via upload

--- homework2.py
+++ homework2.py
@@ -1,8 +1,22 @@
-name = "Pavel"
-print("Name:", name)
-age = 40
-print("Age:", age)
-age = age + 1
-print("New age:", age)
-is_student = True
-print("Is Student:", is_student)
+class Vehicle:
+    vehicle_type = None
+
+
+class Car:
+    price = 1000000
+
+    def horse_powers(self):
+        return self.horse_power
+
+
+class Nissan(Car, Vehicle):
+    vehicle_type = 'Nissan'
+    price = 1100000
+    horse_power = 110
+
+    def horse_powers(self):
+        return None
+
+
+car1 = Nissan()
+print(car1.vehicle_type, car1.price, car1.horse_powers())
