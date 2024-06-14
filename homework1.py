8860cd9eb680b9ba7163321e0349e40a530b65fc
mohapix <166431661+mohapix@users.noreply.github.com>
2024-06-14 06:37:18 +0300

    Add files via upload

--- homework1.py
+++ homework1.py
@@ -1,28 +1,32 @@
-class Car:
-    price = 1000000
+# -*- coding: utf-8 -*-
 
-    def horse_powers(self):
-        return self.horse_power
+from pprint import pprint
 
+text_ =\
+"""My soul is dark - Oh! quickly string
+The harp I yet can brook to hear;
+And let thy gentle fingers fling
+Its melting murmurs o'er mine ear.
+If in this heart a hope be dear,
+That sound shall charm it forth again:
+If in these eyes there lurk a tear,
+'Twill flow, and cease to burn my brain.
 
-class Nissan(Car):
-    price = 1100000
-    horse_power = 110
+But bid the strain be wild and deep,
+Nor let thy notes of joy be first:
+I tell thee, minstrel, I must weep,
+Or else this heavy heart will burst;
+For it hath been by sorrow nursed,
+And ached in sleepless silence, long;
+And now 'tis doomed to know the worst,
+And break at once - or yield to song."""
 
-    def horse_powers(self):
-        return None
+filename = 'homework1.txt'
+file = open(filename, mode='w', encoding='utf8')
+file.write(text_)
+file.close()
 
-
-class Kia(Car):
-    price = 1200000
-    horse_power = 120
-
-    def horse_powers(self):
-        return False
-
-
-car1 = Nissan()
-car2 = Kia()
-
-print(car1.horse_powers())
-print(car2.horse_powers())
+file = open(filename, mode='r', encoding='utf8')
+file_content = file.read()
+file.close()
+pprint(file_content)
