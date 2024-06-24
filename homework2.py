167ea3bd9c59bfa9fcff75c0e50fdcae50dcf241
mohapix <166431661+mohapix@users.noreply.github.com>
2024-06-25 00:39:49 +0300

    Add files via upload

--- homework2.py
+++ homework2.py
@@ -1,45 +1,63 @@
 # -*- coding: utf-8 -*-
-# длина окружности = 2 * радиус * Pi
 
-class MyException(Exception):
-
-    def __init__(self, name, message, info=None):
-        self.name = name
-        self.message = message
-        self.info = info
-
-
-def get_pi():
-    count = 0
-
-    def pi(x, action):
-        nonlocal count
-        if count == 100:
-            raise MyException('ProcessingError', 'устал считать Пи', f'остановился на делителе {x}')
-        if action == '+':
-            count += 1
-            return 1 / x + pi(x + 2, '-')
-        elif action == '-':
-            count += 1
-            return 1 / x - pi(x + 2, '+')
-
-    return 4 * pi(1, '-')
-
-
-def get_radius():
-    user_input = input('Введите радиус окружности: ')
-    if not user_input.isnumeric():
-        raise MyException('InvalidDataError', 'введены неверные данные', f'введено "{user_input}"')
-    else:
-        return int(user_input)
+# Фабрика функций для сложения, вычитания, умножения и деления:
+def create_operation(operation):
+    if operation == "plus":
+        def plus(x, y):
+            return x + y
+        return plus
+    elif operation == "minus":
+        def minus(x, y):
+            return x - y
+        return minus
+    elif operation == "multiply":
+        def multiply(x, y):
+            return x * y
+        return multiply
+    elif operation == "divide":
+        def divide(x, y):
+            try:
+                result = x / y
+            except ZeroDivisionError:
+                return f'Error: Division by zero'
+            else:
+                return round(result, 2)
+        return divide
 
 
-def circle_len():
-    return 2 * get_radius() * get_pi()
+new_func_plus = create_operation('multiply')
+print(new_func_plus(2, 3))
+
+new_func_divide = create_operation('divide')
+print(new_func_divide(18, 9))
+print(new_func_divide(20, 0))
 
 
-try:
-    result = circle_len()
-    print(result)
-except MyException as exc:
-    print(f'Ошибка: {exc.name} - {exc.message}, дополнительная информация: {exc.info}')
+# Лямбда функции с аналогом через def
+my_num_xx2 = lambda x: x ** 2
+print(my_num_xx2(4))
+
+
+def func_num_xx2(num):
+    return num ** 2
+
+
+print(func_num_xx2(4))
+
+
+# Создания вызываемого объекта
+class Rect:
+    def __init__(self, a, b):
+        self.side_a = a
+        self.side_b = b
+
+    def __call__(self):
+        return f'Площадь: {self.side_a * self.side_b}'
+
+    def __repr__(self):
+        return f'Стороны: {self.side_a}, {self.side_b}'
+
+
+my_rect = Rect(2, 4)
+print(my_rect)
+print(my_rect())
