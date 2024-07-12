7dd062c91c7290dfe8a626815555a7a4285f62c0
mohapix <166431661+mohapix@users.noreply.github.com>
2024-07-12 23:50:02 +0300

    Add files via upload

--- homework4.py
+++ homework4.py
@@ -1,35 +1,50 @@
-# -*- coding: utf-8 -*-
+class StepValueError(ValueError):
+    pass
 
-# for root, dirs, files in os.walk(directory):
-#   for file in files:
-#     filepath = ?
-#     filetime = ?
-#     formatted_time = time.strftime("%d.%m.%Y %H:%M", time.localtime(filetime))
-#     filesize = ?
-#     parent_dir = ?
-#     print(f'Обнаружен файл: {file}, Путь: {filepath}, Размер: {filesize} байт,
-#     Время изменения: {formatted_time}, Родительская директория: {parent_dir}')
-#
-# Пример решения:
-#
-# Обнаружен файл: main.py, Путь: ./main.py, Размер: 111 байт,
-# Время изменения: 11.11.1111 11:11, Родительская директория: .
-# Пример кода:
-# https://onlinegdb.com/j1Euu7cvQ
 
-import os
-import time
+class Iterator:
 
-# directory = 'D:/PycharmProjects/homeworks/Module7/'
-# directory = os.path.normpath(directory)
-directory = os.path.dirname(__file__)
+    def __init__(self, start, stop, step=1):
+        if step == 0:
+            raise StepValueError('шаг не может быть равен 0')
+        self.start = start - step   # целое число с которого начинается итерация
+        self.stop = stop            # целое число на котором заканчивается итерация
+        self.step = step            # шаг с которой совершается итерация
+        self.pointer = start        # указывает на текущее число в итерации (изначально start)
 
-for root, dirs, files in os.walk(directory):
-    for file in files:
-        filepath = os.path.join(root, file)
-        filetime = os.path.getmtime(filepath)
-        formatted_time = time.strftime("%d.%m.%Y %H:%M:%S", time.localtime(filetime))
-        filesize = os.path.getsize(filepath)
-        parent_dir = os.path.dirname(filepath)
-        print(f'Обнаружен файл: {file}, Путь: {filepath}, Размер: {filesize} байт, '
-              f'Время изменения: {formatted_time}, Родительская директория: {parent_dir}')
+    def __iter__(self):
+        self.pointer = self.start
+        return self
+
+    def __next__(self):
+        self.pointer += self.step
+        if (self.step > 0 and self.pointer > self.stop) or (self.step < 0 and self.pointer < self.stop):
+            raise StopIteration()
+        return self.pointer
+
+
+try:
+    iter1 = Iterator(100, 200, 0)
+    for i in iter1:
+        print(i, end=' ')
+except StepValueError:
+    print('Шаг указан неверно')
+
+iter2 = Iterator(-5, 1)
+iter3 = Iterator(6, 15, 2)
+iter4 = Iterator(5, 1, -1)
+iter5 = Iterator(10, 1)
+
+
+for i in iter2:
+    print(i, end=' ')
+print()
+for i in iter3:
+    print(i, end=' ')
+print()
+for i in iter4:
+    print(i, end=' ')
+print()
+for i in iter5:
+    print(i, end=' ')
+print()
