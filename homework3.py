e2f49b25805fb2b5d931f69508f297bc3a05d3c4
mohapix <166431661+mohapix@users.noreply.github.com>
2024-07-12 22:43:09 +0300

    Add files via upload

--- homework3.py
+++ homework3.py
@@ -1,33 +1,55 @@
-# -*- coding: utf-8 -*-
+from random import choice
 
-team1_name = 'Мастера кода'
-team2_name = 'Волшебники данных'
-team1_num = 5
-team2_num = 6
-score_1 = 40
-score_2 = 42
-tasks_total = 82
-team1_time = 1552.512
-team2_time = 2153.31451
-time_avg = (team1_time + team2_time) / tasks_total
-challenge_result = 'Победа команды '
+# Lambda-функция
 
-if score_1 > score_2 or score_1 == score_2 and team1_time > team2_time:
-    challenge_result += team1_name
-elif score_1 < score_2 or score_1 == score_2 and team1_time < team2_time:
-    challenge_result += team2_name
-else:
-    challenge_result = 'Ничья!'
+first = 'Мама мыла раму'
+second = 'Рамена мало было'
 
-# Использование %
-print('В команде %s участников: %d !' % (team1_name, team1_num))
-print('Итого сегодня в командах участников: %d и %d !' % (team1_num, team2_num))
+my_func = lambda lit1, lit2: lit1 == lit2
 
-# Использование format()
-print('Команда {} решила задач: {} !'.format(team2_name, score_2))
-print('{name} решили задачи за {time:6.2f} с !'.format(name=team2_name, time=team2_time))
+print(list(map(my_func, first, second)))
+"""
+[False, True, True, False, False, False, False, False, True, False, False, False, False, False]
+"""
 
-# Использование f-строк
-print(f'Команды решили {score_1} и {score_2} задач.')
-print(f'Результат битвы: {challenge_result:-^40s}')
-print(f'Сегодня было решено {tasks_total} задач, в среднем по {time_avg:07.2f} секунды на задачу!')
+
+# Замыкание
+
+
+def get_advanced_writer(file_name):
+
+    def write_everything(*data_set):
+        with open(file_name, mode='w') as file:
+            for line in data_set:
+                file.write(str(line) + '\n')
+
+    return write_everything
+
+
+write = get_advanced_writer('example.txt')
+write('Это строчка', ['А', 'это', 'уже', 'число', 5, 'в', 'списке'])
+"""
+Это строчка
+['А', 'это', 'уже', 'число', 5, 'в', 'списке']
+"""
+
+
+# Метод __call__
+class MysticBall:
+
+    def __init__(self, *words):
+        self.words = words
+
+    def __call__(self):
+        return choice(self.words)
+
+
+first_ball = MysticBall('Да', 'Нет', 'Наверное')
+print(first_ball())
+print(first_ball())
+print(first_ball())
+"""
+Нет
+Наверное
+Наверное
+"""
