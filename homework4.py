0eb2c3aa8dc33be185a75d3af837a1537f6e13ba
mohapix <166431661+mohapix@users.noreply.github.com>
2024-07-28 20:28:45 +0300

    Add files via upload

--- homework4.py
+++ homework4.py
@@ -1,50 +1,89 @@
-class StepValueError(ValueError):
-    pass
+import threading
+import queue
+import time
 
 
-class Iterator:
+class Table:
 
-    def __init__(self, start, stop, step=1):
-        if step == 0:
-            raise StepValueError('шаг не может быть равен 0')
-        self.start = start - step   # целое число с которого начинается итерация
-        self.stop = stop            # целое число на котором заканчивается итерация
-        self.step = step            # шаг с которой совершается итерация
-        self.pointer = start        # указывает на текущее число в итерации (изначально start)
+    def __init__(self, number):
+        self.number: int = number
+        self.is_free: bool = True
 
-    def __iter__(self):
-        self.pointer = self.start
-        return self
-
-    def __next__(self):
-        self.pointer += self.step
-        if (self.step > 0 and self.pointer > self.stop) or (self.step < 0 and self.pointer < self.stop):
-            raise StopIteration()
-        return self.pointer
+    def __call__(self, customer):
+        with customer.lock:
+            self.is_free = False
+            time.sleep(5)
+            self.is_free = True
 
 
-try:
-    iter1 = Iterator(100, 200, 0)
-    for i in iter1:
-        print(i, end=' ')
-except StepValueError:
-    print('Шаг указан неверно')
+class Cafe:
 
-iter2 = Iterator(-5, 1)
-iter3 = Iterator(6, 15, 2)
-iter4 = Iterator(5, 1, -1)
-iter5 = Iterator(10, 1)
+    def __init__(self, tables):
+        self.queue = queue.Queue()
+        self.tables: list = tables
+        self.customers_limit: int = 20
+
+    def customer_arrival(self):
+        n = 1
+        while True:
+            if n <= self.customers_limit:
+                customer = Customer(n)
+                print(f'Посетитель номер {n} прибыл.')
+                self.serve_customer(customer)
+                n += 1
+            elif not self.queue.empty():
+                self.serve_customer()
+
+            if n > self.customers_limit and self.queue.empty() and all(table.is_free for table in self.tables):
+                break
+
+            time.sleep(1)
+
+    def serve_customer(self, customer=None):
+        if any(table.is_free for table in self.tables):
+            if not self.queue.empty():
+                if customer is not None:
+                    self.queue.put(customer)
+                    print(f'Посетитель номер {customer.number} ожидает свободный стол.')
+                customer = self.queue.get()
+
+            for table in self.tables:
+                if table.is_free:
+                    customer.table = table
+                    customer.start()
+                    break
+
+        elif customer is not None:
+            self.queue.put(customer)
+            print(f'Посетитель номер {customer.number} ожидает свободный стол.')
 
 
-for i in iter2:
-    print(i, end=' ')
-print()
-for i in iter3:
-    print(i, end=' ')
-print()
-for i in iter4:
-    print(i, end=' ')
-print()
-for i in iter5:
-    print(i, end=' ')
-print()
+class Customer(threading.Thread):
+
+    def __init__(self, number):
+        super().__init__()
+        self.number: int = number
+        self.lock = threading.Lock()
+        self.table = None
+
+    def run(self):
+        print(f'Посетитель номер {self.number} сел за стол {self.table.number}.')
+        self.table(self)
+        print(f'Посетитель номер {self.number} покушал и ушёл.')
+
+
+# Создаем столики в кафе
+table1 = Table(1)
+table2 = Table(2)
+table3 = Table(3)
+tables = [table1, table2, table3]
+
+# Инициализируем кафе
+cafe = Cafe(tables)
+
+# Запускаем поток для прибытия посетителей
+customer_arrival_thread = threading.Thread(target=cafe.customer_arrival)
+customer_arrival_thread.start()
+
+# Ожидаем завершения работы прибытия посетителей
+customer_arrival_thread.join()
