d3566c0ec9190b77dc47f2a70dd786dba2da167e
mohapix <166431661+mohapix@users.noreply.github.com>
2024-08-10 14:17:29 +0300

    Add files via upload

--- homework5.py
+++ homework5.py
@@ -1,28 +1,31 @@
-"""
-a
-b
-c
-ab
-bc
-abc
-"""
+import multiprocessing
+import datetime
 
 
-def all_variants(text):
-    n = 1
-    while n <= len(text):
-        res = ''
-        for lit in text:
-            res += lit
-            if len(res) > n:
-                res = lit
-                yield res
-            elif len(res) == n:
-                yield res
-                res = lit
-        n += 1
+def read_info(name):
+    all_data = []
+    with open(name, mode='r') as file:
+        line = ' '
+        while line:
+            line = file.readline()
+            all_data.append(line)
 
 
-a = all_variants("abc")
-for i in a:
-    print(i)
+if __name__ == '__main__':
+    files = []
+    for index in range(1, 5):
+        files.append(f'./files/file {index}.txt')
+
+    # линейный вызов
+    start = datetime.datetime.now()
+    for filename in files:
+        read_info(filename)
+    end = datetime.datetime.now()
+    print(end - start)
+
+    # многопроцессорный подход
+    start = datetime.datetime.now()
+    with multiprocessing.Pool(processes=4) as pool:
+        pool.map(read_info, files)
+    end = datetime.datetime.now()
+    print(end - start)
